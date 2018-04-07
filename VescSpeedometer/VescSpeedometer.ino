3aebd8f8944e7fb19d6808a724cdb8b1c84c4a39
ffboard1029 <petertmcnally1@gmail.com>
2018-04-07 14:41:59 -0500

    Send and receive data to the VESC, still working on actually parsing it out

--- VescSpeedometer/VescSpeedometer.ino
+++ VescSpeedometer/VescSpeedometer.ino
@@ -32,11 +32,17 @@ SOFTWARE.
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_FeatherOLED_Custom.h>
 #include <bluefruit.h>
+#include <crc.h>
 
 #define VBAT_PIN          (A7)
 #define VBAT_MV_PER_LSB   (0.73242188F)   // 3.0V ADC range and 12-bit ADC resolution = 3000mV/4096
 #define VBAT_DIVIDER      (0.71275837F)   // 2M + 0.806M voltage divider on VBAT = (2M / (0.806M + 2M))
 #define VBAT_DIVIDER_COMP (1.403F)        // Compensation factor for the VBAT divider
+
+//for calculating speed from rpm
+#define DISPLAY_IMPERIAL_UNITS true   //true for mph on display, false for kmh (can still be toggled with the button)
+#define WHEEL_DIAMETER     90    //in mm EVEN WHEN USING IMPERIAL UNITS!!!!
+#define GEAR_RATIO         1.0f  //1 for hub motors  Motor Teeth / Wheel Teeth for belt motors
 
 #define COMM_GET_VALUES   4   //vesc uart command to get values
 
@@ -44,6 +50,8 @@ Adafruit_FeatherOLED_Custom  oled = Adafruit_FeatherOLED_Custom();
 
 BLEClientDis  clientDis;
 BLEClientUart clientUart;
+bool _imperial;
+bool _sendNewRequest;
 
 int readVBAT(void) 
 {
@@ -70,8 +78,10 @@ int readVBAT(void)
 
 void setup() {
   Serial.begin(9600);
-
-  bleCentralSetup()
+  
+  _imperial = DISPLAY_IMPERIAL_UNITS;
+  
+  bleCentralSetup();
 
   // Setup the OLED display
   oled.init();
@@ -79,30 +89,38 @@ void setup() {
 
   pinMode(VBAT_PIN, INPUT);
   readVBAT();
-  oled.setUnits(false);
-  oled.setConnected(true);
-  //oled.setSpeed(0.0);
+  oled.setUnits(_imperial);
+
+  _sendNewRequest = true;
+  
   oled.display();
 }
 
 void loop() {
+  
   if ( Bluefruit.Central.connected() )
   {
     // Not discovered yet
     if ( clientUart.discovered() )
     {
-      // Discovered means in working state
-      // Get Serial input and send to Peripheral
-      if ( Serial.available() )
+      oled.setConnected(true);
+      if ( _sendNewRequest)
       {
-        delay(2); // delay a bit for all characters to arrive
-        
-        char str[20+1] = { 0 };
-        Serial.readBytes(str, 20);
-        
-        clientUart.print( str );
+        char buff[6 + 1] = {0};
+        if(buildGetValuesPacket(buff, 6))
+        {
+          clientUart.print( buff );
+          _sendNewRequest = false;
+        }
       }
     }
+    {
+      oled.setConnected(false);
+    }
+  }
+  else
+  {
+    oled.setConnected(false);
   }
     // Get a raw ADC reading
   int vbat_raw = readVBAT();
@@ -113,18 +131,46 @@ void loop() {
   // VBAT voltage divider is 2M + 0.806M, which needs to be added back
   float vbat_mv = (float)vbat_raw * VBAT_MV_PER_LSB * VBAT_DIVIDER_COMP;
 
-  //oled.fillRect(77, 0, 128, 8, BLACK);
-  //oled.setBattery(vbat_mv/1000);
-  //oled.renderBattery();
-
   //drawSpeed(0.0);
   //setBTConnected(true);
 
   oled.setSpeed(52.7f);
-  oled.setBattery(2.96f);
+  oled.setBattery(vbat_mv/1000);
   oled.display();
 
-  delay(1500);
+  delay(500);
+}
+
+float rpmToSpeed(float rpm)
+{
+  float circ = WHEEL_DIAMETER * 3.1415926535; //pi
+  float mmph = GEAR_RATIO * 60 * circ;
+  if(_imperial)
+  {
+    return mmph / 1609340.0f;  //mm to miles
+  }
+  else
+  {
+    return mmph / 1000000.0f;  
+  }
+}
+
+bool buildGetValuesPacket(char * buff, int len)
+{
+  if(len < 6)
+  {
+    return false;
+  }
+  buff[0] = 2;  //2 for short packets, 3 for packets over 256 in length
+  buff[1] = 1;  //packet length, one byte for short packets 2 bytes for long packets
+  buff[2] = COMM_GET_VALUES;  //the packet data
+
+  //checksum
+  short checksum = crc16((unsigned char *)buff + 2, 1);
+  buff[3] = (char)(checksum >> 8);
+  buff[4] = (char)(checksum & 0xFF);
+  buff[5] = 3; //stop byte
+  return true;
 }
 
 /*********************************************************************
@@ -257,6 +303,11 @@ void disconnect_callback(uint16_t conn_handle, uint8_t reason)
   Serial.println("Disconnected");
 }
 
+bool foundStart = false;
+int vescPacketLen = 0;
+int totalBytesRead = 0;
+bool foundRPM = false;
+
 /**
  * Callback invoked when uart received data
  * @param uart_svc Reference object to the service where the data 
@@ -264,12 +315,89 @@ void disconnect_callback(uint16_t conn_handle, uint8_t reason)
  */
 void bleuart_rx_callback(BLEClientUart& uart_svc)
 {
+  uint8_t vescResponseData[64] = {0};
   Serial.print("[RX]: ");
   
   while ( uart_svc.available() )
   {
-    Serial.print( (char) uart_svc.read() );
-  }
+    
+    int bytesRead = uart_svc.read(vescResponseData, 64);
+    Serial.print("bytes read: ");
+    Serial.print(bytesRead);
+    if(!foundStart)
+    {
+      if(vescResponseData[0] == 2)
+      {
+        Serial.print(" len:");
+        vescPacketLen = vescResponseData[1];
+        Serial.print(vescPacketLen);
+        foundStart = true;
+      }
+      else if(vescResponseData[0] == 3)
+      {
+        vescPacketLen = vescResponseData[1] << 8 + vescResponseData[2];
+        foundStart = true;
+      }
+      else
+      {
+        Serial.print(" not vesc data?? or some other error...... :/");
+        totalBytesRead += bytesRead;
+        continue;
+      }
+    }
 
+    if(foundStart)
+    {
+      int headerLen = 2;
+      if(vescPacketLen > 256)
+      {
+        headerLen = 3;
+      }
+      if(totalBytesRead + bytesRead < headerLen + 23) // byte location for rpm
+      {
+        totalBytesRead += bytesRead;
+         continue;
+      }
+      //location of rpm is 23 bytes into the actual data packet, and is 4 bytes long
+      else if (totalBytesRead + bytesRead >= headerLen + 23 && !foundRPM)
+      {
+        foundRPM = true;
+        int index = headerLen + 23 - totalBytesRead;
+        Serial.print(index);
+        Serial.print(" ");
+        int rpm = (int)(vescResponseData[index++]) << 24 + (int)(vescResponseData[index++]) << 16 + 
+                  (int)(vescResponseData[index++]) << 8 + (int)(vescResponseData[index]);
+        Serial.print(rpm);
+        Serial.print(" rpm ");
+        Serial.print(rpmToSpeed(rpm));
+        Serial.print(" mph");
+        //oled.setSpeed(rpmToSpeed(rpm);
+      }
+      else if(totalBytesRead + bytesRead >= vescPacketLen + headerLen + 3)
+      {
+        Serial.print(" end? ");
+        int index = vescPacketLen + headerLen + 2 - totalBytesRead;
+        //found the end data
+        if(vescResponseData[index] == 3)
+        {
+          Serial.print(" foundEND ");
+          //_sendNewRequest = true;
+          foundStart = false;
+          totalBytesRead = 0;
+          vescPacketLen = 0;
+          foundRPM = false;
+          continue;
+        }
+        else
+        {
+          //keep going?? this should be an error case so huh...
+          totalBytesRead += bytesRead;
+          continue;
+        }
+      }
+    }
+    totalBytesRead += bytesRead;
+  }
   Serial.println();
+  
 }
